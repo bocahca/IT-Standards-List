1c432d2895204f5546cb5cfc77671d93e0ffd717
oliverleon <phyromontega@gmail.com>
2025-07-17 09:57:52 +0700

    feat: add admin account seeder configuration

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $email = env('ADMIN_EMAIL');
+        $password = env('ADMIN_PASSWORD');
+        if ($email && $password) {
+            User::updateOrCreate(
+                ['email' => $email],
+                [
+                    'name' => 'Admin',
+                    'password' => bcrypt($password),
+                ]
+            );
+        } else {
+            $this->command->error('Please set ADMIN_EMAIL and ADMIN_PASSWORD in your .env file.');
+            return;
+        }
+    }
+}
