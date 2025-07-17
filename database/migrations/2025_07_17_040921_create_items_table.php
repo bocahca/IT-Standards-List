7fe3d65cfd1b41558360f15d71424ea60161dbd5
oliverleon <phyromontega@gmail.com>
2025-07-17 11:50:28 +0700

    refactor: change the logic for categories and item in database migration

--- database/migrations/2025_07_17_040921_create_items_table.php
+++ database/migrations/2025_07_17_040921_create_items_table.php
@@ -11,9 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')
+                ->constrained()
+                ->onDelete('cascade');
             $table->string('name');
+            $table->text('standard')->nullable();
+            $table->text('recommendation')->nullable();
             $table->timestamps();
         });
     }
@@ -23,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('items');
     }
 };
